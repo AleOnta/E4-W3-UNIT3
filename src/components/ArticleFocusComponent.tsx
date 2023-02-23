b8a812ea9a9c9ed2a3c90498c58c0282659ed2ea
Alessandro Ontani <casualotn@gmail.com>
2023-02-23 18:24:24 +0100

    implemented error handling for the two diff. fetch

--- src/components/ArticleFocusComponent.tsx
+++ src/components/ArticleFocusComponent.tsx
@@ -1,10 +1,11 @@
-import { Container, Row, Col, Card } from "react-bootstrap";
+import { Container, Row, Col, Card, Alert } from "react-bootstrap";
 import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { IArticle } from "../interfaces/Article";
 
 const ArticleFocusComponent = () => {
   const [article, setArticle] = useState<IArticle>();
+  const [hasError, setHasError] = useState("");
   const params = useParams();
   const endpoint = "https://api.spaceflightnewsapi.net/v3/articles";
 
@@ -15,43 +16,44 @@ const ArticleFocusComponent = () => {
         const data = await response.json();
         setArticle(data);
       } else {
-        console.log("errore nell'esecuzione della fetch");
+        setHasError("C'è stato un errore nella response della fetch in ArticleFocus");
       }
     } catch (error: any) {
-      console.log("errore fatale nell'esecuzione della fetch");
+      setHasError("Errore fatale nella fetch nell'ArticleFocus, ERROR: " + error.message);
     }
   };
 
   useEffect(() => {
     fetchingArticles();
   }, []);
-  useEffect(() => {
-    console.log(article);
-  }, [article]);
 
   return (
     <Container>
       <Row className="d-flex justify-content-center pt-5">
         <Col xs={12} md={8}>
-          <Card className="focusArticle p-4">
-            <Card.Img variant="top" src={article?.imageUrl} />
-            <Card.Title className="p-2 m-3  focusTitle">{article?.title}</Card.Title>
-            <Card.Body>
-              <Card.Text>{article?.summary}</Card.Text>
-            </Card.Body>
-            <ul className="pt-3">
-              <li>Published at: {article?.publishedAt.toString().slice(0, 10)}</li>
-              <li>Updated at: {article?.updatedAt.toString().slice(0, 10)}</li>
-              <li>
-                Credits to:
-                {
-                  <a href={article?.url} target={"_blank"}>
-                    {article?.newsSite}
-                  </a>
-                }
-              </li>
-            </ul>
-          </Card>
+          {hasError.length > 0 ? (
+            <Alert variant="danger">{hasError}</Alert>
+          ) : (
+            <Card className="focusArticle p-4">
+              <Card.Img variant="top" src={article?.imageUrl} />
+              <Card.Title className="p-2 m-3  focusTitle">{article?.title}</Card.Title>
+              <Card.Body>
+                <Card.Text>{article?.summary}</Card.Text>
+              </Card.Body>
+              <ul className="pt-3">
+                <li>Published at: {article?.publishedAt.toString().slice(0, 10)}</li>
+                <li>Updated at: {article?.updatedAt.toString().slice(0, 10)}</li>
+                <li>
+                  Credits to:
+                  {
+                    <a href={article?.url} target={"_blank"}>
+                      {article?.newsSite}
+                    </a>
+                  }
+                </li>
+              </ul>
+            </Card>
+          )}
         </Col>
       </Row>
     </Container>
--- src/components/HomeComponent.tsx
+++ src/components/HomeComponent.tsx
@@ -1,10 +1,11 @@
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Alert } from "react-bootstrap";
 import ArticleComponent from "./ArticleComponent";
 import { useState, useEffect } from "react";
 import { IArticle } from "../interfaces/Article";
 
 const HomeComponent = () => {
   const [articles, setArticles] = useState<IArticle[]>([]);
+  const [hasError, setHasError] = useState("");
   const endpoint = "https://api.spaceflightnewsapi.net/v3/articles";
 
   const fetchingArticles = async () => {
@@ -14,10 +15,10 @@ const HomeComponent = () => {
         const data = await response.json();
         setArticles(data);
       } else {
-        console.log("errore nell'esecuzione della fetch");
+        setHasError("C'è stato un errore nella response della fetch in HomePage");
       }
     } catch (error: any) {
-      console.log("errore fatale nell'esecuzione della fetch");
+      setHasError("Errore fatale nella fetch della Hompage, ERROR: " + error.message);
     }
   };
 
@@ -27,18 +28,25 @@ const HomeComponent = () => {
   return (
     <Container className="pt-4">
       <Row className="d-flex justify-content-center ">
-        <Col xs={12} md={8} className="text-center">
-          <h2>Read some of our Article!</h2>
-          <p>Go ahead</p>
-        </Col>
-        <Col xs={12} md={8}>
-          {/* {hasError && <Alert variant="danger">C'è stato un errore nel caricamento: {hasError}</Alert>} */}
-          <Row className="d-flex justify-content-center">
-            {articles.map((article) => {
-              return <ArticleComponent article={article} key={article.id} />;
-            })}
-          </Row>
-        </Col>
+        {hasError.length > 0 ? (
+          <Alert variant="danger" className="mt-5 pt-5">
+            {hasError}
+          </Alert>
+        ) : (
+          <>
+            <Col xs={12} md={8} className="text-center">
+              <h2>Read some of our Article!</h2>
+              <p>Go ahead</p>
+            </Col>
+            <Col xs={12} md={8}>
+              <Row className="d-flex justify-content-center">
+                {articles.map((article) => {
+                  return <ArticleComponent article={article} key={article.id} />;
+                })}
+              </Row>
+            </Col>
+          </>
+        )}
       </Row>
     </Container>
   );
